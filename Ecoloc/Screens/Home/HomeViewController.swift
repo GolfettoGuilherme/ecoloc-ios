8594d79c6201023c78fbef9fea854493780e8281
Guilherme Golfetto <guilherme.golfetto@hotmail.com>
2022-11-14 20:00:18 -0300

    added resources and stuff

--- Ecoloc/Screens/Home/HomeViewController.swift
+++ Ecoloc/Screens/Home/HomeViewController.swift
@@ -20,13 +20,31 @@ class HomeViewController: UIViewController {
         stackView.spacing = 16
         stackView.isLayoutMarginsRelativeArrangement = true
         stackView.layoutMargins = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
-        stackView.addArrangedSubview(label)
+        stackView.addArrangedSubview(logoStackView)
         return stackView
     }()
     
-    private lazy var label: UILabel = {
+    private lazy var logoStackView: UIStackView = {
+        let stackview = UIStackView()
+        stackview.translatesAutoresizingMaskIntoConstraints = false
+        stackview.axis = .horizontal
+        stackview.spacing = 16
+//        stackview.addArrangedSubview(logoImage)
+//        stackview.addArrangedSubview(logoLabel)
+        return stackview
+    }()
+    
+    private lazy var logoImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "logo")
+        imageView.tintColor = .systemGreen
+        return imageView
+    }()
+    
+    private lazy var logoLabel: UILabel = {
        let label = UILabel()
-        label.text = "Salve salve familia"
+        label.text = "Ecoloc"
         return label
     }()
     
@@ -36,7 +54,6 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemRed
         view.addSubview(stackView)
         configure()
     }
@@ -51,11 +68,34 @@ extension HomeViewController {
     
     func configure() {
         NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            stackView.topAnchor.constraint(equalTo: view.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            logoImage.widthAnchor.constraint(equalToConstant: 50),
+            logoImage.heightAnchor.constraint(equalToConstant: 50),
         ])
     }
     
 }
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+
+struct ContentViewControllerContainerView: UIViewControllerRepresentable {
+    typealias UIViewControllerType = HomeViewController
+    
+    func makeUIViewController(context: Context) -> UIViewControllerType {
+        return HomeViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
+}
+
+struct ViewController_Preview: PreviewProvider {
+    static var previews: some View {
+        ContentViewControllerContainerView().colorScheme(.light)
+    }
+}
+#endif
